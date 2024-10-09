0bda89f8932d33aeb3faa742869bdb5fa63542a2
Srishti Ahirwar <ahirwar.s@northeastern.edu>
2024-10-09 02:00:46 -0400

    Created VPC, public subnet and IG

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "aws_vpc" "a03_vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name = "a03-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "a03_gateway" {
+  vpc_id = aws_vpc.a03_vpc.id
+
+  tags = {
+    Name = "a03-gateway"
+  }
+}
+
+
+resource "aws_subnet" "a03_public_subnet" {
+  count                   = length(var.public_subnets_cidr_block)
+  vpc_id                  = aws_vpc.a03_vpc.id
+  cidr_block              = var.public_subnets_cidr_block[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zones[count.index]
+  tags = {
+    Name = "a03-public-subnet-${count.index}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,30 @@
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets_cidr_block" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnets_cidr_block" {
+  description = "Private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.255.0/24", "10.0.254.0/24", "10.0.253.0/24"]
+}
+
+variable "availability_zones" {
+  description = "Availability zones for subnets"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
