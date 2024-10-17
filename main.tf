eb38dbcb533a9321070b30b9ff9d399e73c275e4
Srishti Ahirwar <ahirwar.s@northeastern.edu>
2024-10-16 20:54:26 -0400

    Added EC2 and Security Groups

--- main.tf
+++ main.tf
@@ -70,3 +70,79 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private.id
 }
 
+resource "aws_security_group" "this" {
+  vpc_id      = aws_vpc.this.id
+  name        = "${var.vpc_name}-app-sg"
+  description = "Security group for web application EC2 instances"
+
+  tags = {
+    Name = "${var.vpc_name}-app-sg"
+  }
+}
+
+# SSH rule for port 22
+resource "aws_security_group_rule" "ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.this.id
+  cidr_blocks       = var.incoming_traffic
+}
+
+# HTTP rule for port 80
+resource "aws_security_group_rule" "http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.this.id
+  cidr_blocks       = var.incoming_traffic
+}
+
+# HTTPS rule for port 443
+resource "aws_security_group_rule" "https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.this.id
+  cidr_blocks       = var.incoming_traffic
+}
+
+# Custom application rule for port 3000
+resource "aws_security_group_rule" "webapp" {
+  type              = "ingress"
+  from_port         = var.application_port
+  to_port           = var.application_port
+  protocol          = "tcp"
+  security_group_id = aws_security_group.this.id
+  cidr_blocks       = var.incoming_traffic
+}
+
+resource "aws_key_pair" "ec2" {
+  key_name   = "${var.ec2_name}-key"
+  public_key = file(var.public_key_path)
+}
+
+# Creating EC2 Instance
+resource "aws_instance" "ec2" {
+  ami                         = var.golden_ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public[0].id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.this.id]
+  key_name                    = aws_key_pair.ec2.key_name
+  disable_api_termination     = false
+
+  root_block_device {
+    volume_size           = var.root_volume_size
+    volume_type           = var.root_volume_type
+    delete_on_termination = var.root_volume_delete_on_termination
+
+  }
+
+  tags = {
+    Name = "${var.ec2_name}-instance"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -28,5 +28,59 @@ variable "private_subnets" {
 variable "vpc_name" {
   description = "Prefix to identify each VPC and its resources"
   type        = string
-  default     = "a03"
+  default     = "a04"
+}
+
+
+variable "ec2_name" {
+  description = "Prefix to identify each EC2 instance"
+  type        = string
+  default     = "a04"
+}
+
+variable "golden_ami_id" {
+  description = "The AMI ID to use for the EC2 instance"
+  type        = string
+  default     = "ami-0b844a6a7aa2a3aff"
+}
+
+variable "application_port" {
+  description = "Port on which the Node js application runs"
+  type        = number
+}
+
+variable "incoming_traffic" {
+  description = "CIDR for Incoming traffic"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "instance_type" {
+  description = "CIDR for Incoming traffic"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "public_key_path" {
+  description = "Path to the SSH public key file"
+  type        = string
+  default     = "~/.ssh/my_key.pem.pub"
+}
+
+variable "root_volume_size" {
+  description = "Size of the root block device in GB"
+  type        = number
+  default     = 25 # GB
+}
+
+variable "root_volume_type" {
+  description = "Type of the root block device"
+  type        = string
+  default     = "gp2"
+}
+
+variable "root_volume_delete_on_termination" {
+  description = "Whether to delete the root block device on termination"
+  type        = bool
+  default     = true
 }
